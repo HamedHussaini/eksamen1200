faf6e7f7cdc9e701acef1eb2df680c9e2c71a322
dugang7 <64773482+dugang7@users.noreply.github.com>
2022-12-05 11:49:57 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const sr = ScrollReveal ({
+    distance : '45px',
+    duration : 2700,
+    reset : true,
+  })
+
+  sr.reveal('.myimageabout',{ delay:350, origin:'left' })
+  sr.reveal('.about-text-content',{ delay:350, origin:'right' })
+  sr.reveal('.home-image',{ delay:350, origin:'left' })
+  sr.reveal('.home-text-content',{ delay:350, origin:'right' })
+  sr.reveal('.meny-text-content',{ delay:350, origin:'right' })
+  sr.reveal('.reserv-text-content',{ delay:350, origin:'right' })
+
+  sr.reveal('.quality-content',{ delay:350, origin:'left' })
+  sr.reveal('.qulity-image',{ delay:350, origin:'right' })
+  sr.reveal('.gallery-image',{ delay:350, origin:'top' })
+  sr.reveal('.menu-food-content',{ delay:350, origin:'top' })
+  sr.reveal('.menu-food-text',{ delay:350, origin:'bottom' })
+  sr.reveal('.food-main-content',{ delay:350, origin:'bottom' })
+  sr.reveal('.before',{ delay:350, origin:'bottom' })
+  sr.reveal('.main-footer',{ delay:350, origin:'bottom' })
